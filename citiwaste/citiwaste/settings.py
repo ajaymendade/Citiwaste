e99556b53128ce1b10a11c1886421c25b9f3029b
Ajay Mendade <114418817+ajaymendade@users.noreply.github.com>
2023-12-29 18:16:20 +0530

    Update settings.py

--- citiwaste/citiwaste/settings.py
+++ citiwaste/citiwaste/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6-8gon3sk0tgtck(ksg1wpf%^)wd_*_q^szo!*7_y+dwib%62+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://citiwaste.onrender.com']
 
 
 # Application definition
